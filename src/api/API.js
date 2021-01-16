18fa03876f409fd6988fdfcb28b7c6c1d331f1cf
Ismail Omodara <omodara145@gmail.com>
2021-01-16 07:09:14 +0100

    Updated API Url

--- src/api/API.js
+++ src/api/API.js
@@ -1,12 +1,6 @@
 const API_KEY = "e1d21db6"
 const BASE_URL = `https://www.omdbapi.com/?i=tt3896198&apikey=${API_KEY}`
 
-
-// Generate a unique token for storing your nominations.
-let token = localStorage.token
-if (!token)
-  token = localStorage.token = Math.random().toString(36).substr(-8)
-
 export const search = (QUERY) =>
     fetch(`${BASE_URL}&s=${QUERY}`)
         .then(res => res.json())
